befff7b56038f95fef9d1708e28845d096a90bdd
iangudmundsson <egudmundsson@verticalscope.com>
2022-12-21 13:18:54 -0500

    second star solution for day 2

--- day2/scores.py
+++ day2/scores.py
@@ -21,6 +21,24 @@ score_map = {
     },
 }
 
+second_score_map = {
+    "A": {
+        "X": 0 + 3,
+        "Y": 3 + 1, 
+        "Z": 6 + 2
+    },
+    "B": {
+        "X": 0 + 1,
+        "Y": 3 + 2, 
+        "Z": 6 + 3
+    },
+    "C": {
+        "X": 0 + 2,
+        "Y": 3 + 3, 
+        "Z": 6 + 1
+    },
+}
+
 for line in lines:
     if line == "\n":
         break
@@ -28,6 +46,7 @@ for line in lines:
     split = line.split()
     opponent = split[0]
     mine = split[1]
-    total += score_map[opponent][mine]
+    #total += score_map[opponent][mine]
+    total += second_score_map[opponent][mine]
 
 print("total score", total)
